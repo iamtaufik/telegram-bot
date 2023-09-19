78866fdee0564114fd74f14d2eca8ff025719ee1
iamtaufik <muhtopik07@gmail.com>
2023-09-19 10:44:12 +0700

    initial commit

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ Object.defineProperty(exports, '__esModule', { value: true });
 const grammy_1 = require('grammy');
 const express_1 = __importDefault(require('express'));
 const scraper_sosmed_1 = require('@bochilteam/scraper-sosmed');
+const cors_1 = __importDefault(require('cors'));
 require('dotenv').config();
 // Create a bot using the Telegram token
-const bot = new grammy_1.Bot('6378892627:AAE2DeUeKNVs-NF0TyYg5iHhhRbySSJmuKk');
+const bot = new grammy_1.Bot(process.env.TELEGRAM_TOKEN || '');
 // when command is ping reply this servers ping ms
 bot.command('ping', (ctx) => ctx.reply('server uptime: ' + Math.round(process.uptime()) + 'ms'));
 bot.command('download', async (ctx) => {
@@ -18,15 +19,18 @@ bot.command('download', async (ctx) => {
   try {
     const url = (_b = (_a = ctx.msg) === null || _a === void 0 ? void 0 : _a.text) === null || _b === void 0 ? void 0 : _b.split(' ')[1];
     if (url.startsWith('https://vt.tiktok.com/')) {
+      ctx.reply('Please wait...');
       const result = await (0, scraper_sosmed_1.tiktokdl)(url);
       return ctx.replyWithVideo(result.video.no_watermark_hd);
     }
     if (url.startsWith('https://www.facebook.com/')) {
+      ctx.reply('Please wait...');
       const result = await (0, scraper_sosmed_1.facebookdlv2)(url);
       return ctx.replyWithVideo(result.result[0].url);
     }
   } catch (error) {
     console.log(error.message);
+    return ctx.reply(error.message);
   }
 });
 // Suggest commands in the menu
@@ -56,6 +60,7 @@ if (process.env.NODE_ENV === 'production') {
   // Use Webhooks for the production server
   const app = (0, express_1.default)();
   app.use(express_1.default.json());
+  app.use((0, cors_1.default)());
   app.use((0, grammy_1.webhookCallback)(bot, 'express'));
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
